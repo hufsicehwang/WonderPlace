5f82f407fe7f8e430bbcfd4c3a9cf879a433085e
kimtaehyuk1 <98taehyukkim@naver.com>
2021-01-29 00:25:06 +0900

    카카오지도 구현 +전에 했던거 복구 +카테고리 주소 검색 +주소 누르면 그 누른 장소 위치까지 받아짐(주위 카테고리 탐색 가능) +우리가 만든 서버통신 +주소에서 클릭한 좌표 리스트 저장

--- app/src/main/java/com/taehyuk/wonderplace/ApiInterface.java
+++ app/src/main/java/com/taehyuk/wonderplace/ApiInterface.java
@@ -3,6 +3,7 @@ package com.taehyuk.wonderplace;
 import com.taehyuk.wonderplace.model.address_search.AddressSearch;
 import com.taehyuk.wonderplace.model.category_search.CategoryResult;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -16,19 +17,27 @@ import retrofit2.http.Query;
 //밑에는 카카오와 통신하기 위해 헤더즈로 rest api를 밑과같은 형식으로 쓰고 @GET으로 @Query(카카오에서 보내라하는 파라메터들로 이뤄짐)로 요청한다.
 public interface ApiInterface
 {
-    //장소이름으로 검색
+
+
+// 우선 서버 통신 연습
+    @GET("tests")
+    Call<ResponseBody> get_Test(
+            @Query("format") String json
+    );
+
+//    장소이름으로 검색
 //    @Headers("Authorization: KakaoAK 69406d6f78e94450237e9dc116fc151e")
 //    @FormUrlEncoded //Field 쓸때 해줘야되는 통신절차
-//    @POST("세부 URL") //우리가 만든 서버 세부 루트
+//    @POST("") //우리가 만든 서버 세부 루트
 //    Call<CategoryResult> getSearchLocation(
 //            @Field("query") String query,  //Field로 뭐 요청할건지
 //            @Field("size") int size
 //    );
 
-    //카테고리로 검색,
+//    카테고리로 검색,
 //    @Headers("Authorization: KakaoAK 69406d6f78e94450237e9dc116fc151e")
 //    @FormUrlEncoded
-//    @POST("세부 URL")
+//    @POST("")
 //    Call<CategoryResult> getSearchCategory(
 //            @Field("category_group_code") String category_group_code,
 //            @Field("x") String x,
@@ -59,19 +68,19 @@ public interface ApiInterface
 
 
     //장소이름으로 특정위치기준으로 검색
-    @GET("v2/local/search/keyword.json")
-    Call<CategoryResult> getSearchLocationDetail(
-            @Header("Authorization") String token,
-            @Query("query") String query,
-            @Query("x") String x,
-            @Query("y") String y,
-            @Query("size") int size
-    );
-
-    @Headers("Authorization: KakaoAK 69406d6f78e94450237e9dc116fc151e")
-    @GET("v2/local/search/address.json")
-    Call<AddressSearch> getSearchAddress(
-            @Query("query") String query,
-            @Query("size") int size
-    );
+//    @GET("v2/local/search/keyword.json")
+//    Call<CategoryResult> getSearchLocationDetail(
+//            @Header("Authorization") String token,
+//            @Query("query") String query,
+//            @Query("x") String x,
+//            @Query("y") String y,
+//            @Query("size") int size
+//    );
+//
+//    @Headers("Authorization: KakaoAK 69406d6f78e94450237e9dc116fc151e")
+//    @GET("v2/local/search/address.json")
+//    Call<AddressSearch> getSearchAddress(
+//            @Query("query") String query,
+//            @Query("size") int size
+//    );
 }
--- app/src/main/java/com/taehyuk/wonderplace/MainActivity.java
+++ app/src/main/java/com/taehyuk/wonderplace/MainActivity.java
@@ -1,21 +1,38 @@
 package com.taehyuk.wonderplace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 //import net.daum.android.map.MapActivity;
+import com.taehyuk.wonderplace.model.category_search.CategoryResult;
+import com.taehyuk.wonderplace.model.category_search.Document;
+
+import net.daum.mf.map.api.MapCircle;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static net.daum.mf.map.api.MapPoint.mapPointWithGeoCoord;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button search_btn;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +40,41 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//화면 못움직이게 세로고정
         setContentView(R.layout.activity_main);
 
-
-        search_btn = (Button) findViewById(R.id.search_btn);
-
-        search_btn.setOnClickListener(new View.OnClickListener()
-        {
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        Call<ResponseBody> call = apiInterface.get_Test("json");
+        call.enqueue(new Callback<ResponseBody>() { //실행하는 메소드
             @Override
-            public void onClick(View view)
-            {
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                try {
+                    Log.e("D_Test", response.body().string());
+                    result = response.body().string();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
-                Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                startActivity(intent);
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                result = "error!!";
+                Log.e("D_Test","페일!");
             }
         });
+
+
+
+
+//        search_btn = (Button) findViewById(R.id.search_btn);
+//
+//        search_btn.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View view)
+//            {
+//
+//                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
 //        MapView mapView = new MapView(this);
 //
